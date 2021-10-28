f19fac9a22cbca0d80a20bc55d57d5c97f296fc4
HauptMachine\vonGostev <gostev.pavel@physics.msu.ru>
2021-10-28 16:39:43 +0300

    Fix psqueezed_vacuum import

--- qpip/stat/__init__.py
+++ qpip/stat/__init__.py
@@ -1,6 +1,6 @@
 from ._pstatistics import (ppoisson, pfock, pthermal,
                            pthermal_photonadd, pthermal_photonsub,
-                           psqueezed_coherent1, psqueezed_vacuumM,
+                           psqueezed_coherent1, psqueezed_vacuum,
                            phyper_poisson, pthermal_polarized,
                            pcompound_poisson)
 from ._qstatistics import qthermal_polarized, qthermal_unpolarized
